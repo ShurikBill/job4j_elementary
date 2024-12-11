efffddc07b1fc0067ec8feb4249def5720214d4c
Александр <ShurikBill@yandex.ru>
2024-12-11 13:38:33 +0300

    Загрузка файлов на github [#505269]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main (String [] args){
+        System.out.println("Bilichenko Aleksandr");
+        System.out.println("13.02.1997");
+    }
+}
