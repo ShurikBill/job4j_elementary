b5e9c1c5c76715fb755d2c235eb18cc5c3f62a24
Александр <ShurikBill@yandex.ru>
2024-12-12 18:42:53 +0300

    Разбор первой программы по "взрослому" [#505151 #525172]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main (String [] args){
+    public static void main(String[] args) {
         System.out.println("Bilichenko Aleksandr");
         System.out.println("13.02.1997");
     }
