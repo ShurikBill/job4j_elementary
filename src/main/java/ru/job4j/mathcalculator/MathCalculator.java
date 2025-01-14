0a33e8b4d9b0d331a0f1b40cb59462388225ea47
Что такое import [#505278] <larinand97@gmail.com>
2025-01-14 20:42:20 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/ru/job4j/mathcalculator/MathCalculator.java
+++ src/main/java/ru/job4j/mathcalculator/MathCalculator.java
@@ -8,16 +8,19 @@ public class MathCalculator {
         return ru.job4j.math.MathFunction.sum(first, second)
                 + ru.job4j.math.MathFunction.multiply(first, second);
     }
+
     public static double sumAndSum(double first, double second) {
         return ru.job4j.math.MathFunction.sum(first, second)
                 + ru.job4j.math.MathFunction.sum(first, second);
     }
+
     public static double allOperations(double first, double second) {
         return ru.job4j.math.MathFunction.sum(first, second)
                 + ru.job4j.math.MathFunction.multiply(first, second)
                 + ru.job4j.math.MathFunction.subtraction(first, second)
                 + ru.job4j.math.MathFunction.division(first, second);
     }
+
         public static void main(String[] args) {
         System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
         System.out.println("Результат расчета равен: " + sumAndSum(10, 20));
