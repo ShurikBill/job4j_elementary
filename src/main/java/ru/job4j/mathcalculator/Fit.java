4bcdb58476f4079a93e615123d88b9bc13719ac0
Что такое import [#505278] <larinand97@gmail.com>
2025-01-15 17:16:30 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/ru/job4j/mathcalculator/Fit.java
+++ src/main/java/ru/job4j/mathcalculator/Fit.java
@@ -2,12 +2,10 @@ package ru.job4j.mathcalculator;
 
 public class Fit {
     public static double manWeight(short height) {
-    // Formula for man's weight: (height in cm - 100) * 1.15
     return (height - 100) * 1.15;
 }
 
     public static double womanWeight(short height2) {
-        // Formula for woman's weight: (height in cm - 110) * 1.15
         return (height2 - 110) * 1.15;
     }
 
