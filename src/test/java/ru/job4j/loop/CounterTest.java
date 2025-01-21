147e7fc16a8b9242b1603a256b9e9a21a64813bf
Что такое import [#505278] <larinand97@gmail.com>
2025-01-21 05:43:56 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.loop;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+import ru.job4j.loop.Counter;
+
+public class CounterTest {
+
+    @Test
+    void whenStart1AndFinish10Then55() {
+        int start = 1, finish = 10;
+        double expected = 55;
+        double output = Counter.sum(start, finish);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenStart5AndFinish7Then18() {
+        int start = 10, finish = 1;
+        double expected = 0;
+        double output = Counter.sum(start, finish);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+}
