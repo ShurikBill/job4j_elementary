801c06fd669c06a8629a9946a9c0b81cdb72459c
Что такое import [#505278] <larinand97@gmail.com>
2025-01-21 05:46:01 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -3,7 +3,7 @@ package ru.job4j.loop;
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
-        for (start = start;  start <=finish; start++) {
+        for (start = start;  start <= finish; start++) {
             sum  = sum + start;
         }
         return sum;
