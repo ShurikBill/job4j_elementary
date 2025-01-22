b85b6ed9e3d8d0b40d4b9e4fa68abab60cd4a7ea
Что такое import [#505278] <larinand97@gmail.com>
2025-01-22 04:58:12 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class Factorial {
+    public static int calculate(int number) {
+        int sum = 1;
+        for (int i = sum; i <= number; i++) {
+            sum = sum * i;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculate(6));
+        System.out.println(calculate(8));
+        System.out.println(calculate(10));
+    }
+}
