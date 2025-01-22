dcad6fa54b9bcd14607f8b5a486f0dfbbfc9c753
Что такое import [#505278] <larinand97@gmail.com>
2025-01-22 15:05:00 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,20 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        ivan = 90;
+        nik = 100;
+        while (ivan < nik) {
+            System.out.println(month);
+            ivan *= 3;
+            nik *= 2;
+            month++;
+        }
+        return month;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calc (90,100));
+    }
+}
