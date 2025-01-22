a84756699bc77e7bc9d95dc59877c22180d879d7
Что такое import [#505278] <larinand97@gmail.com>
2025-01-22 17:22:52 +0300

    настройка java

--- src/test/java/ru/job4j/loop/FitnessTest.java
+++ src/test/java/ru/job4j/loop/FitnessTest.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class FitnessTest {
-
-
+    
     @Test
     void whenIvanGreatNik() {
         int ivan = 95;
