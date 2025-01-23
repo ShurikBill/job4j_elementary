64c220156c2c7a533ceed83da4b81bb2b29eb0fb
Что такое import [#505278] <larinand97@gmail.com>
2025-01-23 22:58:33 +0300

    выполнение задания

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -2,8 +2,8 @@ package ru.job4j.loop;
 
 public class Fitness {
     public static int calc(int ivan, int nik) {
-        int month = 1;
-        while (ivan < nik) {
+        int month = 0;
+        while (ivan <= nik) {
             System.out.println(month);
             ivan *= 3;
             nik *= 2;
