717d1f534b4c8fd459475cafafc0fac58cda8a5d
Что такое import [#505278] <larinand97@gmail.com>
2025-01-23 22:48:02 +0300

    выполнение задания

--- src/main/java/ru/job4j/loop/Factorial.java
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -3,10 +3,10 @@ package ru.job4j.loop;
 public class Factorial {
     public static int calculate(int number) {
         int sum = 1;
-        for (int i = sum; i <= number; i++) {
+        for (int i = 2 ; i <= number; i++) {
             sum = sum * i;
         }
-        return sum;
+        return sum ;
     }
 
     public static void main(String[] args) {
