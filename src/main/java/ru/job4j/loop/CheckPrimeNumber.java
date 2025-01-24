f42d8f9745c51ff392bba89d8fd165eddb21405b
Что такое import [#505278] <larinand97@gmail.com>
2025-01-24 21:29:03 +0300

    Простое число (оператор прерывания break) [#505214]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -1,15 +1,17 @@
 package ru.job4j.loop;
 
 public class CheckPrimeNumber {
-    public static boolean check(int number){
-        int i = 2;
-        for (i =2; i <= number; i++) {
-            if (number % i == 0)
-                return false;
-            {
-                break;
-            }
-        }
+    public static boolean check(int number) {
+        if (number < 2) {
             return false;
+        }
+        boolean rsl = true;
+        for (int i = 2; i < number; i++){
+             if (number % i == 0){
+                 rsl = false;
+                 break;
+             }
+        }
+        return rsl;
     }
 }
