189dfc21febfa278bbf36855fedbb69d07c18d9f
Что такое import [#505278] <larinand97@gmail.com>
2025-01-29 15:53:52 +0300

    Массивы и цикл for [#505223]

--- src/main/java/ru/job4j/array/ArrayLoop.java
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -4,7 +4,7 @@ public class ArrayLoop {
     public static void main(String[]args) {
         int[] number = new int[5];
         for (int index = 0; index < number.length; index++) {
-            number[index] = index * 2 +3;
+            number[index] = index * 2 + 3;
         }
         for (int index = 0; index < number.length; index++) {
             System.out.println(number[index]);
