0bc286ee800325e85de000d64240a65310b14fb4
Что такое import [#505278] <larinand97@gmail.com>
2025-02-24 21:33:47 +0300

    Сравнить последние элементы двух массивов [#505234 #533695]

--- src/main/java/ru/job4j/array/EqualLast.java
+++ src/main/java/ru/job4j/array/EqualLast.java
@@ -2,13 +2,6 @@ package ru.job4j.array;
 
 public class EqualLast {
     public static boolean check(int[] left, int[] right) {
-        boolean result = false;
-        for (int index = 0; index < right.length; index++) {
-            if (left[left.length - 1] == right[right.length - 1]) {
-                result = true;
-                break;
-            }
-        }
-        return result;
+        return left[left.length - 1] == right[right.length - 1];
     }
 }
