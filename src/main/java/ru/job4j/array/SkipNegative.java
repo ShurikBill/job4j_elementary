ab502d017186d64e99de28c36490244ce345a7ff
Что такое import [#505278] <larinand97@gmail.com>
2025-03-03 21:21:46 +0300

    6.6.3. Двумерный массив. If. [#505244]]

--- src/main/java/ru/job4j/array/SkipNegative.java
+++ src/main/java/ru/job4j/array/SkipNegative.java
@@ -4,8 +4,8 @@ public class SkipNegative {
     public static int[][] skip(int[][] array) {
         for (int row = 0; row < array.length; row++) {
             for (int cell = 0; cell < array[row].length; cell++) {
-                if (array[row][cell] <= 0) {
-                    row == 0;
+                if (array[row][cell] < 0) {
+                    array[row][cell] = 0;
                 }
             }
         }
