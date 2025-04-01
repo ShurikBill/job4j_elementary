1f81f1edd9c8d73ec642d83c705f12006ed851b1
Что такое import [#505278] <larinand97@gmail.com>
2025-04-01 16:38:47 +0300

    Экзамен.Есть отсортированный одномерный массив от меньшего к большему. ЗАДАНИЕ используя метод двух указателей отсортировать массив в порядке возрастания, а так же возвести значения в квадрат (2-ую степень)

--- src/test/java/ru/job4j/array/ExamTest.java
+++ src/test/java/ru/job4j/array/ExamTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ExamTest {
     @Test
     void getSquareAndRevers6() {
-        int[] array = {6, 5, 4, 3, 2, 1, -1, -5};
+        int[] array = {-5, -1, 1, 2, 3, 4, 5, 6};
         int[] result = Exam.getSquareAndRevers(array);
         int[] expected = {1, 1, 4, 9, 16, 25, 25, 36};
         assertThat(result).isEqualTo(expected);
@@ -15,7 +15,7 @@ class ExamTest {
 
     @Test
     void getSquareAndRevers5() {
-        int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10};
+        int[] array = {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int[] result = Exam.getSquareAndRevers(array);
         int[] expected = {0, 1, 1, 4, 4, 9, 9, 16, 16, 25, 25, 36, 36, 49, 49, 64, 64, 81, 81, 100, 100};
         assertThat(result).isEqualTo(expected);
