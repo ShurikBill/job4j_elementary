f76c357dcaeb8ea8351140ec87b1645438827377
Что такое import [#505278] <larinand97@gmail.com>
2025-04-01 16:05:28 +0300

    Экзамен.Есть отсортированный одномерный массив от большего к меньшему. ЗАДАНИЕ используя метод двух указателей отсортировать массив в порядке возрастания, а так же возвести значения в квадрат (2-ую степень)

--- src/main/java/ru/job4j/array/Exam.java
+++ src/main/java/ru/job4j/array/Exam.java
@@ -2,15 +2,22 @@ package ru.job4j.array;
 
 public class Exam {
     public static int[] getSquareAndRevers(int[] array) {
+        int[] result = new int[array.length];
         int i = 0;
         int j = array.length - 1;
+        int index = array.length - 1;
         while (i <= j) {
-            int temp = (int) Math.pow(array[i], 2);
-            array[i] = (int) Math.pow(array[j], 2);
-            array[j] = temp;
-            i++;
-            j--;
+            double i2 = Math.pow(array[i], 2);
+            double j2 = Math.pow(array[j], 2);
+            if (i2 > j2) {
+                result[index] = (int) i2;
+                i++;
+            } else {
+                result[index] = (int) j2;
+                j--;
+            }
+            index--;
         }
-        return array;
+        return result;
     }
 }
--- src/test/java/ru/job4j/array/ExamTest.java
+++ src/test/java/ru/job4j/array/ExamTest.java
@@ -7,17 +7,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ExamTest {
     @Test
     void getSquareAndRevers6() {
-        int[] array = {6, 5, 4, 3, 2, 1};
+        int[] array = {6, 5, 4, 3, 2, 1, -1, -5};
         int[] result = Exam.getSquareAndRevers(array);
-        int[] expected = {1, 4, 9, 16, 25, 36};
+        int[] expected = {1, 1, 4, 9, 16, 25, 25, 36};
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
     void getSquareAndRevers5() {
-        int[] array = {5, 4, 3, 2, 1};
+        int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10};
         int[] result = Exam.getSquareAndRevers(array);
-        int[] expected = {1, 4, 9, 16, 25};
+        int[] expected = {0, 1, 1, 4, 4, 9, 9, 16, 16, 25, 25, 36, 36, 49, 49, 64, 64, 81, 81, 100, 100};
         assertThat(result).isEqualTo(expected);
     }
 }
