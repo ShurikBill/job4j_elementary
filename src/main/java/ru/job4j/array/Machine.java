a393c65183134cc3b5b3694fb1077ecd33714bd7
Что такое import [#505278] <larinand97@gmail.com>
2025-04-01 16:17:44 +0300

    6.8.1. Жадный алгоритм. Сдача в кофе машине [#505250 #537453]

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -7,10 +7,11 @@ public class Machine {
         int[] coins = {10, 5, 2, 1};
         int[] result = new int[100];
         int size = 0;
+        money -= price;
         for (int coin : coins) {
-            while (coin <= money - price) {
+            while (coin <= money) {
                 result[size++] = coin;
-                price += coin;
+                money -= coin;
             }
         }
         return Arrays.copyOf(result, size);
